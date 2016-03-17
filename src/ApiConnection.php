a63688572d5a4ac44a60e1bd264a45cdf040fd2d
Chris Endcliffe <cpbrain@gmail.com>
2016-03-17 20:47:41 +0000

    Some classes added

---
+++ src/ApiConnection.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * User: Chris Endcliffe
+ * Date: 17/03/16
+ * Time: 20:42
+ */
+
+namespace LittleGuru\Guru;
+
+
+class ApiConnection
+{
+
+    protected $publicKey;
+
+    protected $privateKey;
+
+    protected $lastError;
+
+    public function __construct($publicKey, $privateKey)
+    {
+
+        $this->privateKey;
+
+        $this->publicKey;
+
+
+    }
+
+    public function call($url, $params)
+    {
+
+
+
+    }
+
+    private function handleErrors()
+    {
+
+    }
+
+}
