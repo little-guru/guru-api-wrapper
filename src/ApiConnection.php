50828c3d30aa53bd6c43aba567b8b094b06c7815
Chris Endcliffe <cpbrain@gmail.com>
2016-03-21 20:43:22 +0000

    More func changes

--- src/ApiConnection.php
+++ src/ApiConnection.php
@@ -11,26 +11,55 @@ namespace LittleGuru\Guru;
 class ApiConnection
 {
 
-    protected $publicKey;
-
-    protected $privateKey;
+    protected $token;
 
     protected $lastError;
 
-    public function __construct($publicKey, $privateKey)
+    public function __construct( $token)
     {
 
-        $this->privateKey;
+       $this->token = $token;
 
-        $this->publicKey;
 
 
     }
 
-    public function call($url, $params)
+    private function connection()
     {
 
+        $ch = curl_init();
 
+        curl_setopt($ch, CURLOPT_URL, $this->url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, 'Authorization: '. $this->token);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+
+        return $ch;
+
+    }
+
+    public function get($url, array $params)
+    {
+
+        $ch = $this->connection();
+
+        if ($params)
+        {
+            $url = $url . '?' . $this->prepareQuery($params);
+
+            curl_setopt($ch, CURLOPT_URL, $url);
+        }
+
+
+        $result = curl_exec($ch);
+
+        return $result;
+
+    }
+
+    private function prepareQuery(array $params){
+
+        return http_build_query($params);
 
     }
 
